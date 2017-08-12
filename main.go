b0164f33671166c7cf276c188e5661112ee9899e
Ethan Bogart <ethan.bogart@yahoo.com>
2017-08-12 16:38:24 -0400

    Going back with environment variables

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
-var DB_USERNAME string
-var DB_PASSWORD string
-var DB_NAME string
+import (
+  "os"
+)
 
 func main() {
   a := App{}
   a.Initialize(
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_NAME)
+    os.Getenv("PW_DB_USERNAME"),
+    os.Getenv("PW_DB_PASSWORD"),
+    os.Getenv("PW_DB_NAME"))
 
   a.Run(":8080")
 }
