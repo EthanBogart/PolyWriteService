c7d44a3b2814c327707ddee183c6a68d327a0fec
Ethan Bogart <ethan.bogart@yahoo.com>
2017-08-12 19:50:22 -0400

    Added a couple sql queries

--- model.go
+++ model.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"database/sql"
+)
+
 type user struct {
-	Id    int    `json:"id"`
+	ID    int    `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
+}
+
+func (u *user) getUserByID(db *sql.DB) error {
+	return db.QueryRow("SELECT name, email FROM users WHERE id=$1",
+		u.ID).Scan(&u.Name, &u.Email)
+}
+
+func (u *user) createUser(db *sql.DB) error {
+  err := db.QueryRow(
+    "INSERT INTO user(id, name, email) VALUES(uuid(), $1, $2) RETURNING id",
+    u.Name, u.Email).Scan(&u.ID)
+
+  if err != nil {
+    return err
+  }
+
+  return nil
 }
